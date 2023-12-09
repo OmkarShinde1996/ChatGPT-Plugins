f5856ff3cce60f40a64649fb62aa20c02ed04e69
Omkar Shinde <109081005+OmkarShinde1996@users.noreply.github.com>
2023-12-09 19:00:44 +0530

    Update route.ts

--- app/api/langchain/tool/agent/edge/route.ts
+++ app/api/langchain/tool/agent/edge/route.ts
@@ -5,10 +5,86 @@ import { EdgeTool } from "../../../../langchain-tools/edge_tools";
 import { OpenAI } from "langchain/llms/openai";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
+// async function handle(req: NextRequest) {
+//   if (req.method === "OPTIONS") {
+//     return NextResponse.json({ body: "OK" }, { status: 200 });
+//   }
+//   try {
+//     const authResult = auth(req);
+//     if (authResult.error) {
+//       return NextResponse.json(authResult, {
+//         status: 401,
+//       });
+//     }
+
+//     const encoder = new TextEncoder();
+//     const transformStream = new TransformStream();
+//     const writer = transformStream.writable.getWriter();
+//     const agentApi = new AgentApi(encoder, transformStream, writer);
+
+//     const reqBody: RequestBody = await req.json();
+//     const authToken = req.headers.get("Authorization") ?? "";
+//     const token = authToken.trim().replaceAll("Bearer ", "").trim();
+
+//     const apiKey = await agentApi.getOpenAIApiKey(token);
+//     const baseUrl = await agentApi.getOpenAIBaseUrl(reqBody.baseUrl);
+
+//     const model = new OpenAI(
+//       {
+//         temperature: 0,
+//         modelName: reqBody.model,
+//         openAIApiKey: apiKey,
+//       },
+//       { basePath: baseUrl },
+//     );
+//     // console.log("model = ", model);
+//     const embeddings = new OpenAIEmbeddings(
+//       {
+//         openAIApiKey: apiKey,
+//       },
+//       { basePath: baseUrl },
+//     );
+//     // console.log("embeddings = ", embeddings);
+//     var dalleCallback = async (data: string) => {
+//       var response = new ResponseBody();
+//       response.message = data;
+//       await writer.ready;
+//       await writer.write(
+//         encoder.encode(`data: ${JSON.stringify(response)}\n\n`),
+//       );
+//     };
+//     // console.log("dalleCallback = ", dalleCallback);
+//     var edgeTool = new EdgeTool(
+//       apiKey,
+//       baseUrl,
+//       model,
+//       embeddings,
+//       dalleCallback,
+//     );
+//     var edgeTools = await edgeTool.getCustomTools();
+//     var tools = [...edgeTools];
+//     return await agentApi.getApiHandler(req, reqBody, tools);
+
+//   } catch (e) {
+//     return new Response(JSON.stringify({ error: (e as any).message }), {
+//       status: 500,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
+
+// export const GET = handle;
+// export const POST = handle;
+
+// export const runtime = "edge";
+
+
+//Modified code below
 async function handle(req: NextRequest) {
   if (req.method === "OPTIONS") {
     return NextResponse.json({ body: "OK" }, { status: 200 });
   }
+
   try {
     const authResult = auth(req);
     if (authResult.error) {
@@ -17,17 +93,12 @@ async function handle(req: NextRequest) {
       });
     }
 
-    const encoder = new TextEncoder();
-    const transformStream = new TransformStream();
-    const writer = transformStream.writable.getWriter();
-    const agentApi = new AgentApi(encoder, transformStream, writer);
-
     const reqBody: RequestBody = await req.json();
     const authToken = req.headers.get("Authorization") ?? "";
     const token = authToken.trim().replaceAll("Bearer ", "").trim();
 
-    const apiKey = await agentApi.getOpenAIApiKey(token);
-    const baseUrl = await agentApi.getOpenAIBaseUrl(reqBody.baseUrl);
+    const apiKey = await AgentApi.getOpenAIApiKey(token);
+    const baseUrl = await AgentApi.getOpenAIBaseUrl(reqBody.baseUrl);
 
     const model = new OpenAI(
       {
@@ -37,41 +108,26 @@ async function handle(req: NextRequest) {
       },
       { basePath: baseUrl },
     );
-    // console.log("model = ", model);
     const embeddings = new OpenAIEmbeddings(
       {
         openAIApiKey: apiKey,
       },
       { basePath: baseUrl },
     );
-    // console.log("embeddings = ", embeddings);
-    var dalleCallback = async (data: string) => {
-      var response = new ResponseBody();
-      response.message = data;
-      await writer.ready;
-      await writer.write(
-        encoder.encode(`data: ${JSON.stringify(response)}\n\n`),
-      );
-    };
-    // console.log("dalleCallback = ", dalleCallback);
-    var edgeTool = new EdgeTool(
-      apiKey,
-      baseUrl,
-      model,
-      embeddings,
-      dalleCallback,
-    );
-    var edgeTools = await edgeTool.getCustomTools();
-    var tools = [...edgeTools];
-    // var resp = await agentApi.getApiHandler(req, reqBody, tools);
-    console.log("response is : ",await agentApi.getApiHandler(req, reqBody, tools))
-    return await agentApi.getApiHandler(req, reqBody, tools);
-    
-    //Modified code below
-    
-    //=======================================
+
+    const edgeTool = new EdgeTool(apiKey, baseUrl, model, embeddings);
+    const edgeTools = await edgeTool.getCustomTools();
+
+    const agentApi = new AgentApi();
+    const response = await agentApi.getApiHandler(req, reqBody, edgeTools);
+
+    return NextResponse.json(JSON.parse(response), {
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (e) {
-    return new Response(JSON.stringify({ error: (e as any).message }), {
+    return NextResponse.json({
+      error: (e as any).message,
+    }, {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
